b36fe0cfb782bc3d30f2d6d94184f001b2e96d52
k-ef <faurillokarledward@gmail.com>
2026-09-14 19:45:34 +0800

    Basic structure of the progrgram

--- test/createwindow.c
+++ test/createwindow.c
@@ -1,8 +1,57 @@
+#define UNICODE
+#define _UNICODE
 #include <stdio.h>
 #include <stdlib.h>
 #include <windows.h>
+#include <WinDef.h>
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) 
+{
+    const wchar_t *CLASS_NAME = L"Pomodoro"; 
+
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND pomodoro_hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        L"Pomodoro Timer",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        CW_USEDEFAULT, CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+
+    if (pomodoro_hwnd == NULL) {
+        return 0;
+    }
+
+    ShowWindow(pomodoro_hwnd, nCmdShow);
+
+    MSG msg = {0};
+
+    while (GetMessage(&msg, NULL, 0, 0) > 0) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
     
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+        default: return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
 }
 
