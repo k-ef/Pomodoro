84681c6b953dabb61c4a07ecf7ae8a29feb6230c
k-ef <faurillokarledward@gmail.com>
2026-09-24 20:07:00 +0800

    Added and changed function prototypes

--- test/pomodoro.h
+++ test/pomodoro.h
@@ -1,11 +1,12 @@
 #ifndef POMODORO_H_
 #define POMODORO_H_
 
-void RegisterTimerWindowClass(const wchar_t *class_name, HINSTANCE hInstance);
-HWND CreateTimerWindow(const wchar_t *class_name, const wchar_t *caption, int x_pos, int y_pos, int x_size, int y_size, HINSTANCE hInstance, HWND parent_hwnd);
+void RegisterChildWindowClass(const wchar_t *class_name, HINSTANCE hInstance, LRESULT(*fPtr)(HWND, UINT, WPARAM, LPARAM));
+HWND CreateChildWindow(const wchar_t *class_name, const wchar_t *caption, int x_pos, int y_pos, int x_size, int y_size, HINSTANCE hInstance, HWND parent_hwnd);
 void RegisterMainWindowClass(const wchar_t *class_name, HINSTANCE hInstance);
 HWND CreateMainWindow(const wchar_t *class_name, const wchar_t *caption, int x_size, int y_size, HINSTANCE hInstance);
 LRESULT CALLBACK TimerWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK NoteWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 LRESULT CALLBACK MainWindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 #endif
